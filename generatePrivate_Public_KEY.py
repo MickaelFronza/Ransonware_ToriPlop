fb46bc79293d1bfbeb8a70e567ef06947f44a20e
Mickael <mickael.fronza@gmail.com>
2023-04-21 10:51:29 -0300

    Geração da chave privada e chave publica

--- generatePrivate_Public_KEY.py
+++ generatePrivate_Public_KEY.py
@@ -1,4 +1,4 @@
-from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import RSA
 
 key = RSA.generate(2048)
 privateKey = key.export_key()
@@ -7,13 +7,10 @@ publicKey = key.publickey().export_key()
 #
 with open('private.pem', 'wb') as f:
     f.write(privateKey)
-
+    
 with open('public.pem', 'wb') as f:
     f.write(publicKey)
-
+    
 print('Private key saved to private.pem')
 print('Private key saved to public.pem')
 print('Done')
-
-# Gerar as Chaves Publicas e Privadas no bash do Git
-# Command Line python generatePrivate_Public_KEY.py
